51f85a55a767d5625ccb7f3e2ebeac9aac21ea6c
MochamadFarhanAbdullah <112877057+MochamadFarhanAbdullah@users.noreply.github.com>
2022-09-05 21:54:54 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{ //Ini tugas matkul Dasar Pemrograman pertemuan 1
+    cout << "Mochamad Farhan Abdullah" << endl;
+    cout << "Nim : A11.2022.14145" << endl;
+    cout << "Semarang, Ngaliyan" << endl;
+    cout << "SMA N 7 Semarang" << endl ;
+    return 0;
+}
