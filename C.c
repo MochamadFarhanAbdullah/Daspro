22ea697409eb11584111dfa86a046b6efbbed8c7
MochamadFarhanAbdullah <112877057+MochamadFarhanAbdullah@users.noreply.github.com>
2022-09-19 15:51:35 +0700

    Add files via upload

---
+++ C.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <iostream>
+
+using namespace st
+
+int main ()
+{
+    int x,y,z;
+
+    printf ("Angka kehidupan: ");
+    scanf ("%d, &x");
+    printf ("Angka favorit: ");
+    scanf ("%d,&y");
+
+
+    return 0;
+}
